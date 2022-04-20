80a00342cf99548532c595965fc74e00ab56fccd
farouk <farouksouei@gmail.com>
2022-04-20 22:00:30 +0200

    "first commit"

---
+++ main.py
@@ -0,0 +1,31 @@
+testeur = True
+prduits = ("Espadris","Chausettes")
+users = {}
+
+def rechercheProduit():
+    print("Hello World!\n")
+
+def login(user,mdp):
+    print("login succesful\n")
+
+def register(user,mdp):
+    print("register succesfull !!\n")
+
+
+if __name__ == "__main__":
+    print("Enter 1 to Register if you don't have an account\n")
+    print("Enter 2 to Login if you have an account\n")
+    while(testeur == True):
+        var = int(input("enter your choice"))
+        if var == 1:
+            user = input("Enter your new username")
+            mdp = input("Enter your new password")
+            register(user,mdp)
+            testeur = False
+            break
+        elif var == 2:
+            user = input("Enter your username")
+            mdp = input("Enter your password")
+            login(user,mdp)
+        else:
+            print("Input invalid")
